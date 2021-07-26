85384afeedc30338d834b3e90bccac4eed2d5d5d
C+- <hulk_228@inbox.ru>
2021-07-26 18:48:38 +0400

    Add song counter, timer and progress bar.

--- SongGuesser/PlayForm.Designer.cs
+++ SongGuesser/PlayForm.Designer.cs
@@ -28,15 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayForm));
             this.wmp = new AxWMPLib.AxWindowsMediaPlayer();
             this.buttonNext = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonContinue = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.labelPlayer1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.labelPlayer2 = new System.Windows.Forms.Label();
+            this.labelSongCounter = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.wmp)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,65 +66,99 @@
             this.buttonNext.UseVisualStyleBackColor = true;
             this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
             // 
-            // button1
+            // buttonPause
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(329, 351);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 87);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Пауза";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPause.Location = new System.Drawing.Point(329, 351);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(172, 87);
+            this.buttonPause.TabIndex = 2;
+            this.buttonPause.Text = "Пауза";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
             // 
-            // button2
+            // buttonContinue
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(529, 351);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(230, 87);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Продолжить";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonContinue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonContinue.Location = new System.Drawing.Point(529, 351);
+            this.buttonContinue.Name = "buttonContinue";
+            this.buttonContinue.Size = new System.Drawing.Size(230, 87);
+            this.buttonContinue.TabIndex = 3;
+            this.buttonContinue.Text = "Продолжить";
+            this.buttonContinue.UseVisualStyleBackColor = true;
+            this.buttonContinue.Click += new System.EventHandler(this.buttonContinue_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(91, 35);
+            this.label1.Location = new System.Drawing.Point(67, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(101, 29);
             this.label1.TabIndex = 4;
             this.label1.Text = "Игрок 1";
             // 
-            // label2
+            // labelPlayer1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(123, 82);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 29);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "0";
+            this.labelPlayer1.AutoSize = true;
+            this.labelPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPlayer1.Location = new System.Drawing.Point(99, 83);
+            this.labelPlayer1.Name = "labelPlayer1";
+            this.labelPlayer1.Size = new System.Drawing.Size(26, 29);
+            this.labelPlayer1.TabIndex = 5;
+            this.labelPlayer1.Text = "0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(630, 35);
+            this.label3.Location = new System.Drawing.Point(606, 36);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(101, 29);
             this.label3.TabIndex = 6;
             this.label3.Text = "Игрок 2";
             // 
-            // label4
+            // labelPlayer2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(664, 82);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(26, 29);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "0";
+            this.labelPlayer2.AutoSize = true;
+            this.labelPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPlayer2.Location = new System.Drawing.Point(640, 83);
+            this.labelPlayer2.Name = "labelPlayer2";
+            this.labelPlayer2.Size = new System.Drawing.Size(26, 29);
+            this.labelPlayer2.TabIndex = 7;
+            this.labelPlayer2.Text = "0";
+            // 
+            // labelSongCounter
+            // 
+            this.labelSongCounter.AutoSize = true;
+            this.labelSongCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSongCounter.Location = new System.Drawing.Point(345, 83);
+            this.labelSongCounter.Name = "labelSongCounter";
+            this.labelSongCounter.Size = new System.Drawing.Size(29, 31);
+            this.labelSongCounter.TabIndex = 8;
+            this.labelSongCounter.Text = "0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(72, 270);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(635, 43);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(287, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "песен осталось";
             // 
             // PlayForm
             // 
@@ -127,17 +166,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SongGuesser.Properties.Resources.main;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelSongCounter);
+            this.Controls.Add(this.labelPlayer2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelPlayer1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonContinue);
+            this.Controls.Add(this.buttonPause);
             this.Controls.Add(this.buttonNext);
             this.Controls.Add(this.wmp);
             this.Name = "PlayForm";
             this.Text = "PlayForm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.PlayForm_FormClosed);
+            this.Load += new System.EventHandler(this.PlayForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.wmp)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -148,11 +191,15 @@
 
         private AxWMPLib.AxWindowsMediaPlayer wmp;
         private System.Windows.Forms.Button buttonNext;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonContinue;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelPlayer1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelPlayer2;
+        private System.Windows.Forms.Label labelSongCounter;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- SongGuesser/PlayForm.cs
+++ SongGuesser/PlayForm.cs
@@ -25,16 +25,48 @@ namespace SongGuesser
             n = rand.Next(0, Challenge.songs.Count);
             wmp.URL = Challenge.songs[n];
             Challenge.songs.RemoveAt(n);
+            labelSongCounter.Text = Challenge.songs.Count.ToString();
+        }
+
+        private void PlayForm_Load(object sender, EventArgs e)
+        {
+            labelSongCounter.Text = Challenge.songs.Count.ToString();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = Challenge.gameDuration;
+            progressBar1.Value = 0;
         }
 
         private void buttonNext_Click(object sender, EventArgs e)
         {
             playRandomSong();
+            timer1.Start();
         }
 
         private void PlayForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             wmp.Ctlcontrols.stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value++;
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                timer1.Stop();
+                wmp.Ctlcontrols.stop();
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            wmp.Ctlcontrols.pause();
+        }
+
+        private void buttonContinue_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            wmp.Ctlcontrols.play();
         }
     }
 }
